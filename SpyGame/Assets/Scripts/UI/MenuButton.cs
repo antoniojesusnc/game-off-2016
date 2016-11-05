1716e1e536370c6ad6cb217db3414c6ce4a43db7
Christopher Ozdoba <christopher.ozdoba@gmail.com>
2016-11-05 20:39:25 +0400

    Add basic menu scene

---
+++ SpyGame/Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;  
+using System.Collections;  
+using UnityEngine.EventSystems;  
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public Text theText;
+	private Color originalColor;
+
+	void Start() 
+	{
+		theText = GetComponentInChildren<Text> ();
+		originalColor = theText.color;
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		theText.color = Color.red; //Or however you do your color
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		theText.color = originalColor; //Or however you do your color
+	}
+
+	public void SetNextScene(string nextSceneName) {
+		Debug.Log ("Setting next scene name to " + nextSceneName);
+		SpyGame.SceneController.SwitchScene (nextSceneName);
+	}
+
+	public void Quit() {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
