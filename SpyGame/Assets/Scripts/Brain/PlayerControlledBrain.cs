e543a56e2c751a53ddb5cd888f7a8c03bfd7df22
Christopher Ozdoba <christopher.ozdoba@gmail.com>
2016-11-07 23:07:43 +0400

    Add first placeholder for Brain and Thinker

---
+++ SpyGame/Assets/Scripts/Brain/PlayerControlledBrain.cs
@@ -0,0 +1,28 @@
+﻿namespace SpyGame
+{
+	using UnityEngine;
+	using System.Collections;
+
+	[CreateAssetMenu(menuName="Brains/Player Controlled")]
+	public class PlayerControlledBrain : Brain
+	{
+
+		public int PlayerNumber;
+
+		private string _movementAxisName;
+
+		private string _turnAxisName;
+
+
+		public void OnEnable()
+		{
+			_movementAxisName = "Vertical";
+			_turnAxisName     = "Horizontal";
+		}
+
+		public override void Think(Thinker thinker)
+		{			
+			/// !!!TODO: implement me
+		}
+	}
+}
---
+++ SpyGame/Assets/Scripts/Brain/Thinker.cs
@@ -0,0 +1,39 @@
+﻿namespace SpyGame
+{
+	using UnityEngine;
+	using System.Collections;
+
+	public class Thinker : MonoBehaviour 
+	{
+		public Brain brain;
+
+		void Awake()
+		{
+			enabled = false;
+		}
+
+		void OnEnable()
+		{
+			if (!brain)
+			{
+				enabled = false;
+				return;
+			}
+
+			brain.Initialize(this);
+		}
+
+		void Update ()
+		{
+			brain.Think(this);
+		}
+
+		public void Setup(Transform spawnPoint)
+		{
+			transform.position = spawnPoint.position;
+			transform.rotation = spawnPoint.rotation;
+
+			enabled = true;
+		}
+	}
+}
