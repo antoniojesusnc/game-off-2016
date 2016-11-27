1c796399bb2ada687502c48662c079d8f8d05262
Christopher Ozdoba <christopher.ozdoba@gmail.com>
2016-11-27 19:58:24 +0400

    Add 3rd-Person enemy controller

--- SpyGame/Assets/Scripts/Brain/PatrollingBrain.cs
+++ SpyGame/Assets/Scripts/Brain/PatrollingBrain.cs
@@ -11,10 +11,10 @@ namespace SpyGame
 	public class PatrollingBrain : Brain 
 	{
 		public List<Transform> waypoints;
-		public float speed = 1.0f;
+		public float speed = 0.5f;
 		private int currentWaypoint = 0;
 		 
-		private Character m_character;
+		private ThirdPersonCharacter m_character;
 		private Transform m_Cam;
 
 		public override void Initialize(Thinker thinker) 
@@ -23,7 +23,7 @@ namespace SpyGame
 			currentWaypoint = 0;
 
 			thinker.transform.position = waypoints [currentWaypoint].position;
-			m_character = thinker.GetComponentInParent<Character> ();
+			m_character = thinker.GetComponentInParent<ThirdPersonCharacter> ();
 		}
 
 		public override void Think(Thinker thinker)
@@ -31,7 +31,7 @@ namespace SpyGame
 			Vector3 sourcePosition = thinker.transform.position;
 			Vector3 targetPosition = waypoints [GetNextWaypoint()].position;
 
-			Debug.Log ("Cam.forward: " + m_Cam.forward.ToString());
+//			Debug.Log ("Cam.forward: " + m_Cam.forward.ToString());
 
 			Vector3 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
 			Vector3 dir = (targetPosition - sourcePosition).normalized;
@@ -40,7 +40,7 @@ namespace SpyGame
 			m_character.Move(dir, false, false);
 
 			float dist = Vector3.Distance (thinker.transform.position, targetPosition);
-			if (dist < 3) {
+			if (dist < 2) {
 				currentWaypoint = GetNextWaypoint();
 			}
 		}
