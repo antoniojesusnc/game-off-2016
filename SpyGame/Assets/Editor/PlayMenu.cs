c67451f06db223a6a5e2e21aec7a9fa50499a476
Christopher Ozdoba <christopher.ozdoba@gmail.com>
2016-11-27 14:22:26 +0400

    Create Play-shortcut to start game from StartScene

---
+++ SpyGame/Assets/Editor/PlayMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PlayMenu : MonoBehaviour {
+
+	[MenuItem("Play/StartScene")]
+	public static void PlayFromPrelaunchScene() {
+		if (EditorApplication.isPlaying == true) {
+			EditorApplication.isPlaying = false;
+			return;
+		}
+
+		EditorApplication.SaveCurrentSceneIfUserWantsTo();
+		EditorApplication.OpenScene("Assets/Scenes/StartScene.unity");
+
+		EditorApplication.isPlaying = true;
+	}
+}
